b0c06bd7457f438037943807b99874b45144aee1
manbradcalf <ben.medcalf@gmail.com>
2020-06-02 18:30:27 -0400

    add search bar and bio

---
+++ src/components/ArtistDetail/Bio.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Text, View, StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+    infoText: {
+        fontSize: 12,
+        color: 'blue',
+        fontWeight: 'bold'
+    },
+    headerText: {
+        paddingTop: 24,
+        fontSize: 24,
+        color: 'black',
+        fontWeight: 'bold'
+    }
+});
+
+export const Bio: React.FC<string> = (bioText) => {
+    return (
+        <View style={styles.root}>
+            <Text style={styles.headerText}>Biography</Text>
+            <Text style={styles.infoText}>{bioText}</Text>
+        </View>
+    )
+}
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { SearchBar } from 'react-native-elements';
+import React from 'react';
+
+export class SearchBarArtie extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search: string) => {
+    this.setState({ search });
+  };
+
+  render() {
+    const { search } = this.state;
+
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={this.updateSearch}
+        platform="ios"
+        value={search}
+      />
+    );
+  }
+}
